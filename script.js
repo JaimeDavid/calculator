585d011941cbcae1db135ee39c16f8ae03adbdf5
JaimeDavid <7207667+JaimeDavid@users.noreply.github.com>
2024-08-10 14:47:28 -0500

    added js basic math operations

---
+++ script.js
@@ -0,0 +1,18 @@
+// basic math functions.
+ function add(x, y){
+    return x + y;
+ }
+
+ function subtract(x,y){
+    return x-y;
+ }
+
+ function multiply(x, y){
+    return x * y;
+ }
+
+ function division(x, y){
+
+    if (y == 0) return "Are you trying to end the world!!!!";
+    return x / y;
+ }
