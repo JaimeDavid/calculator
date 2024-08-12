b053fc6df0c4d2d5e3a4e658609689464093d1cd
JaimeDavid <7207667+JaimeDavid@users.noreply.github.com>
2024-08-11 20:59:04 -0500

    added js functionality

--- script.js
+++ script.js
@@ -16,4 +16,40 @@
     return x / y;
  }
 
- 
+ //operation variables
+ let firstNumber = '';
+ let secondNumber = '';
+ let operator = '';
+
+ //operate function
+ function operate(firstNumber, secondNumber, operator){
+   switch (operator){
+      case 'division':
+         return division(firstNumber, secondNumber);
+      
+      case 'multiplication':
+         return multiply(firstNumber, secondNumber);
+
+      case 'subtraction':
+         return subtract(firstNumber, secondNumber);
+
+      case 'addition':
+         return add(firstNumber, secondNumber);      
+   }
+ }
+//places numeric calculator data in the appropiate variable
+ function numericalData(){
+   if ((operator === '')){
+      firstNumber += this.textContent;
+      screen.textContent = firstNumber;
+   } else{
+      secondNumber += this.textContent; 
+   }
+ }
+
+ // DOM manipulation
+ let numbers = document.querySelectorAll('.number')
+ let screen = document.getElementById('results');
+
+ numbers.forEach((number) =>
+   number.addEventListener('click', numericalData));
