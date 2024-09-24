c4b066cc561dcbeeff077ccbdb2f107cc70f0c1e
JaimeDavid <7207667+JaimeDavid@users.noreply.github.com>
2024-09-23 23:19:58 -0500

    working file

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// calculator Variables
+let currentValue = '';
+let storedValue = '';
+let operator = '';
+let result = ''
+
+
+
+
 // basic math functions.
 function add(x, y){
     return x + y;
@@ -17,78 +26,74 @@ function division(x, y){
  }
 
  //operation variables
-let firstNumber = '';
-let secondNumber = '';
-let operator = '';
-let result = '';
-let currentValue ='';
+
+
 
  //operate function
-function operate(firstNumber, secondNumber, operator){
+function operate(storedValue, currentValue, operator){
    switch (operator){
       case 'division':
-         result = division(firstNumber, secondNumber);
-         firstNumber = result;
-         secondNumber = '';
+         result = division(storedValue, currentValue);
          break;
       
       case 'multiplication':
-         result = multiply(firstNumber, secondNumber);
-         firstNumber = result;
-         secondNumber = '';
+         result = multiply(storedValue, currentValue);
          break;
 
       case 'subtraction':
-         result = subtract(firstNumber, secondNumber);
-         firstNumber = result;
-         secondNumber = '';
+         result = subtract(storedValue, currentValue);
          break;
 
       case 'addition':
-         result = add(firstNumber, secondNumber);
-         firstNumber = result;
-         secondNumber = '';
+         result = add(storedValue, currentValue);
          break;      
    }
  }
+
+
+
 // assigns number value to current value
 function screenValue(numberElement){ 
-   currentValue += numberElement.textContent;   
+   if( result !== ''){
+      currentValue = '';
+      storedValue = result;
+      result = '';
+      currentValue += numberElement.textContent; 
+   } else if( currentValue === 0){
+      currentValue = '';
+      currentValue += numberElement.textContent; 
+
+   }else{
+      currentValue += numberElement.textContent;  
+   }
+ 
 }
 
-// upoates calculator screen
+// updates calculator screen
 function screenText(){
-   screen.textContent = currentValue;
- }
-
-
-//places numeric calculator data in the appropiate variable
-function numericalData(numericalElement){
-   if ((operator === '')){
-      firstNumber = parseInt(currentValue);
-
-   } else{
-      secondNumber = parseInt(currentValue);       
-      
+   if (result !== ''){
+      screen.textContent = result;
+   }else {
+      screen.textContent = currentValue;
    }
  }
 
- // places operator data in the appropiate variable
+
+ // places operator data in the appropiate variable and resets variables depending on the case
 function operatorData(operatorElement){ 
+      if(result !== ''){
+        storedValue = currentValue;
+        currentValue 
+      }
       operator = operatorElement.id;
-      currentValue = '';
+      storedValue = currentValue;
+      currentValue = ''
    }
 // resets the screen to 0
 function reset(){
-      firstNumber = 0;
-      screen.textContent = firstNumber;
-      firstNumber = '';
-   }
-// performs operations without using the equals button
-function secondOperation(){
-      if((operator !== '') && (firstNumber !== '') && (secondNumber !== '')){
-         operate(parseInt(firstNumber), parseInt(secondNumber), operator)
-      }
+      currentValue = 0;
+      storedValue = '';
+      screen.textContent = currentValue;
    }
 
 
@@ -105,19 +110,19 @@ let clear = document.getElementById('ac');
 numbers.forEach((number) =>
    number.addEventListener('click', () => {
       screenValue(number);
-      numericalData(number);
       screenText();
    }));
 
 operators.forEach((operatorElement) =>
-   operatorElement.addEventListener('click', ()=>{
+   operatorElement.addEventListener('click', ()=>{      
       operatorData(operatorElement);
-      secondOperation();      
+      operate(parseInt(storedValue), parseInt(currentValue),operator);
+    
    })
 );
 
 equals.addEventListener('click', ()=>{
-   operate(parseInt(firstNumber), parseInt(secondNumber),operator);
+   operate(parseInt(storedValue), parseInt(currentValue),operator);
    screenText();
 });
 
